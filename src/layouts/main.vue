feff8fb5074c0853f1d24adb039c9daebdc4ae6f
xfy196 <xfy196@outlook.com>
2022-04-04 22:26:22 +0800

    更新路由动画的一些细节

--- src/layouts/main.vue
+++ src/layouts/main.vue
@@ -1,16 +1,25 @@
 <template>
   <a-layout-content class="layout__container">
-    <!-- <router-view /> -->
     <router-view v-slot="{ Component }">
       <keep-alive v-if="route.meta.keepAlive">
-        <transition name="fade">
-          <component :is="Component" :key="route.name" />
-        </transition>
+        <Suspense>
+          <template #default>
+            <transition name="fade">
+              <component :is="Component" :key="route.name" />
+            </transition>
+          </template>
+          <template #fallback>Loading ...</template>
+        </Suspense>
       </keep-alive>
       <template v-else>
-        <transition name="fade">
-          <component :is="Component" :key="route.name" />
-        </transition>
+        <Suspense>
+          <template #default>
+            <transition name="fade">
+              <component :is="Component" :key="route.name" />
+            </transition>
+          </template>
+          <template #fallback>Loading ...</template>
+        </Suspense>
       </template>
     </router-view>
   </a-layout-content>
@@ -22,20 +31,19 @@ const route = useRoute();
 <style lang="scss" scoped>
 .layout__container {
   height: calc(100vh - 64px);
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: all 300ms cubic-bezier(0.55, 0, 0.1, 1);
-    position: absolute;
-    backface-visibility: hidden;
-    opacity: 1;
-  }
+  overflow: hidden;
+  .fade-leave-active,
   .fade-enter-active {
-    opacity: 0;
-    transform: translate3d(100%, 0, 0);
+    transition: all 0.5s;
   }
-  .fade-leave-active {
+
+  .fade-enter {
     opacity: 0;
-    transform: translate3d(-3%, 0, 0);
+    transform: translateX(-30px);
+  }
+  .fade-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
   }
 }
 </style>
