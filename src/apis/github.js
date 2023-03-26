88c3cf09079d1a89b6a8ac85932edc4f193ce7a4
xfy196 <xfy196@outlook.com>
2023-03-26 16:27:15 +0800

    :bug: fix 一些问题

--- src/apis/github.js
+++ src/apis/github.js
@@ -1,47 +1,57 @@
-import request from "../utils/request"
-import path from "path-browserify"
+import request from "../utils/request";
+import path from "path-browserify";
 // 通过token获取的用户信息
 export const requestUserInfo = async () => {
-    return request({
-        url: "/user"
-    })
-}
+  return request({
+    url: "/user",
+  });
+};
 // 获取仓库列表
 export const requestRepos = (params) => {
-    return request({
-        url: `/users/${params.login}/repos`
-    })
-} 
+  return request({
+    url: `/users/${params.login}/repos`,
+    params: {
+      page: params.page,
+      per_page: params.prePage,
+    },
+  });
+};
 // 获取选择仓库的分支
 export const requetsBranches = (params) => {
-    return request({
-        url: `/repos/${params.login}/${params.repo}/branches`
-    })
-}
+  return request({
+    url: `/repos/${params.login}/${params.repo}/branches`,
+  });
+};
 // 获取分支下的文件目录
 export const requestBranchCatalogue = async (params) => {
-    return request({
-        url: path.join(`/repos/${params.login}/${params.repo}/contents/${params.dir}`),
-        params: params.query
-    })
-}
+  return request({
+    url: path.join(
+      `/repos/${params.login}/${params.repo}/contents/${params.dir}`
+    ),
+    params: params.query,
+  });
+};
 // 文件上传 同时支持新增和修改
 export const requestUpload = async (body) => {
-    return request({
-        url: path.join(`/repos/${body.login}/${body.repo}/contents/${body.dirs}/${body.filename}`),
-        method: "PUT",
-        data: {
-            branch: body.branch,
-            message: body.message,
-            content: body.content
-        }
-    })
-}
+  return request({
+    url: path.join(
+      `/repos/${body.login}/${body.repo}/contents/${body.dirs}/${body.filename}`
+    ),
+    method: "PUT",
+    data: {
+      branch: body.branch,
+      message: body.message,
+      content: body.content,
+    },
+  });
+};
 // 删除文件
 export const requestDeleteFile = async (body) => {
-    return request({
-        url: path.join(`/repos/${body.login}/${body.repo}/contents/${body.dirs}/${body.filename}`),
-        data: body.data,
-        method: "DELETE"
-    })
-}
+  return request({
+    url: path.join(
+      `/repos/${body.login}/${body.repo}/contents/${body.dirs}/${body.filename}`
+    ),
+    data: body.data,
+    method: "DELETE",
+  });
+};
